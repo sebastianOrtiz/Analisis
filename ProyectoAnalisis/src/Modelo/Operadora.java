728a4d7ae24e49b87c363f5f8c6a455e62f22406
sebastianOrtiz <sebastianortiz989@gmail.com>
2015-11-15 15:47:06 -0500

    Particionando

--- ProyectoAnalisis/src/Modelo/Operadora.java
+++ ProyectoAnalisis/src/Modelo/Operadora.java
@@ -7,47 +7,97 @@ package Modelo;
 
 import java.awt.Point;
 import java.util.LinkedList;
-import javafx.scene.shape.Line;
 
 /**
  *
  * @author Ambrosio
  */
 public class Operadora {
-    private LinkedList<Point> puntos;
-    private LinkedList<Line> lineas;
-    private int altoPanel;
-    private int anchoPanel;
 
-    public Operadora(int altoP, int anchoP) {
-        this.altoPanel = altoP;
-        this.anchoPanel = anchoP;
+    private LinkedList<Point> puntos;
+    private LinkedList<Linea> lineas;
+    private Arbol arbol;
+
+    public Operadora() {
         this.puntos = new LinkedList<>();
         this.lineas = new LinkedList<>();
     }
-    
-    public Line[] lineasInciales(){
-        Line[] lineas = new Line[2];
-        lineas[0] = new Line(this.puntos.getFirst().getX(), 0, this.puntos.getFirst().getX(), this.altoPanel);
-        lineas[1] = new Line(0, this.puntos.getFirst().getY(), this.anchoPanel, this.puntos.getFirst().getY());
-        return lineas;
-    }  
-    
-    public void adicionarPunto(Point p){
-        getPuntos().add(p);
+
+    public void definirPuntos(LinkedList<Point> puntos) {
+        this.puntos = puntos;
     }
 
-    /**
-     * @return the puntos
-     */
-    public LinkedList<Point> getPuntos() {
-        return puntos;
+    private LinkedList<Point> sacarPorcion(LinkedList<Point> pts, int indIni, int indFin) {
+        LinkedList<Point> subPts = new LinkedList<Point>();
+        for (int i = indIni; i < indFin; i++) {
+            subPts.add(pts.get(i));
+        }
+        return subPts;
+    }
+
+    private LinkedList<Point> organizarPuntos(LinkedList<Point> pts, boolean orientacion) {
+        if (!this.puntos.isEmpty()) {
+            if (orientacion) {
+                for (int i = 0; i < pts.size(); i++) {
+                    for (int j = i + 1; j < pts.size(); j++) {
+                        if (pts.get(i).getX() > pts.get(j).getX()) {
+                            Point aux = pts.get(i);
+                            pts.set(i, pts.get(j));
+                            pts.set(j, aux);
+                        }
+                    }
+                }
+            } else {
+                for (int i = 0; i < pts.size(); i++) {
+                    for (int j = i + 1; j < pts.size(); j++) {
+                        if (pts.get(i).getY() > pts.get(j).getY()) {
+                            Point aux = pts.get(i);
+                            pts.set(i, pts.get(j));
+                            pts.set(j, aux);
+                        }
+                    }
+                }
+            }
+            return pts;
+        }
+        return null;
+    }
+
+    public void particionar(LinkedList<Point> puntos, int width, int height) {
+        particionarRec(puntos, 0, 0, width, height, true);
+    }
+
+    private void particionarRec(LinkedList<Point> pts, int iniWidth, int iniHeight, int finWidth, int finHeight, boolean orientacion) {
+        Linea l = null;
+        if (pts.size() > 1) {
+            LinkedList<Point> puntosOdenados = organizarPuntos(pts, orientacion);
+            int mitad = puntosOdenados.size() / 2;
+            if (orientacion) {
+                l = new Linea(new Point((int) puntosOdenados.get(mitad).getX(), iniHeight), new Point((int) puntosOdenados.get(mitad).getX(), finHeight));
+                particionarRec(sacarPorcion(pts, 0, mitad - 1), iniWidth, iniHeight, (int) puntosOdenados.get(mitad).getX(), finHeight, !orientacion);
+                particionarRec(sacarPorcion(pts, mitad + 1, puntosOdenados.size()), (int) puntosOdenados.get(mitad).getX(), iniHeight, finWidth, finHeight, !orientacion);
+            } else {
+                l = new Linea(new Point(iniWidth, (int) puntosOdenados.get(mitad).getY()), new Point(finWidth, (int) puntosOdenados.get(mitad).getY()));
+                particionarRec(sacarPorcion(pts, 0, mitad), iniWidth, iniHeight, finWidth, (int) puntosOdenados.get(mitad).getY(), !orientacion);
+                particionarRec(sacarPorcion(pts, mitad + 1, puntosOdenados.size()), iniWidth, (int) puntosOdenados.get(mitad).getY(), finWidth, finHeight, !orientacion);
+            }
+            this.getLineas().add(l);
+
+        } else if (pts.size() == 1) {
+            if (orientacion) {
+                l = new Linea(new Point((int) pts.getFirst().getX(), iniHeight), new Point((int) pts.getFirst().getX(), finHeight));
+            } else {
+                l = new Linea(new Point(iniWidth, (int) pts.getFirst().getY()), new Point(finWidth, (int) pts.getFirst().getY()));
+            }
+            this.getLineas().add(l);
+        }
+
     }
 
     /**
      * @return the lineas
      */
-    public LinkedList<Line> getLineas() {
+    public LinkedList<Linea> getLineas() {
         return lineas;
     }
 }
